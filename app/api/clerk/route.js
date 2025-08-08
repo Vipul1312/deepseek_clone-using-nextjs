1c8713ca071b08c7a49b4915ba78051833fb3f52
vipul1312 <vipulgupta1312002@gmail.com>
2025-08-08 12:05:06 +0530

    updated message

--- app/api/clerk/route.js
+++ app/api/clerk/route.js
@@ -2,10 +2,11 @@ import { Webhook } from "svix";
 import connectDB from "@/config/db";
 import User from "@/models/User";
 import { NextRequest } from "next/server";
+import { headers } from "next/headers";
 
 export async function POST(req) {
   const wh = new Webhook(process.env.SIGNING_SECRET);
-  const headerPayload = await header();
+  const headerPayload = await headers();
   const svixHeaders = {
     "svix-id": headerPayload.get("svix-id"),
     "svix-timestamp": headerPayload.get("svix-timestamp"),
